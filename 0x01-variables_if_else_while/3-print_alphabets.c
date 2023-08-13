803bcbbf802e3edd00cc7c8af491fd84bfa0c91a
David Ogola <davidogola10@gmail.com>
2023-08-13 00:37:40 -0700

    3-cr

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+
 /**
  * main - main function
  *
  * Return: 0 Always
  */
+
 int main(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
-	for (i = 'A'; i <= 'Z'; I++)
+	for (i = 'A'; i <= 'Z'; i++)
 		putchar(i);
 	putchar('\n');
-	
 	return (0);
 }
 
