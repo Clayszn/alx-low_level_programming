44b830f1f865118ed9c43cca1558f154cca61db8
David Ogola <davidogola10@gmail.com>
2023-08-13 01:48:36 -0700

    6-crane

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	unsigned char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
