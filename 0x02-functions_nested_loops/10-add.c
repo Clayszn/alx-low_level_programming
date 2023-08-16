4fd38712b4a718dd0d998452b3a1a795a0085548
David Ogola <davidogola10@gmail.com>
2023-08-16 16:30:29 -0700

    fib levels

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *add - entry point for the code
+  *@a: value to be added
+  *@b: added to value a
+  *
+  *Return: Value of (a + b)
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
