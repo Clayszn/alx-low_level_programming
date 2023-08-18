bde023c23afeefee5ccc9e100279d574787a2347
David Ogola <davidogola10@gmail.com>
2023-08-18 14:58:02 -0700

    _putchar

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/** _isupper -  checks for uppercase character
+ *
+ * Return: 1 on sucess
+ */
+
+int _isupper(int c);
+{
+	char c;
+
+	for (c = 'A' c <= 'Z' c++)
+		_putchar(c);
+	return (0);
+}
