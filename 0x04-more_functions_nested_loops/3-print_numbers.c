5735d4d5bce56d782e99b65d17f8de1106075357
David Ogola <davidogola10@gmail.com>
2023-08-18 23:47:01 -0700

    last

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_numbers - print numbers
+*
+*Return: 0 Always
+*/
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+}
