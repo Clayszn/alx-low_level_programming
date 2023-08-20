5ad3446a9598dc0ceb37e8bbc0bd4196e21896ad
David Ogola <davidogola10@gmail.com>
2023-08-20 09:36:17 -0700

    edit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,62 +1,17 @@
-#include "_putchar.c"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-*print_numbers - print numbers
-*
-*Return: 0 Always
-*/
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
-void print_numbers(void)
-{
-        int a;
-
-        for (a = '0'; a <= '9'; a++)
-        {
-                _putchar(a);
-        }
-        _putchar('\n');
-
-}
-
-
-/**
-*print_most_numbers - prints
-*Return: void
-*/
-
-void print_most_numbers(void)
-{
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
-	}
-	_putchar('\n');
-}
-
-/**
- * more_numbers - nmbr fct
- *
- * Return: no return
- */
-
-void more_numbers(void)
-{
-
-int i, j;
-        for (i = 1; i <= 10; i++)
-        {
-                for (j = 0; j <= 14; j++)
-                {
-                        if (j >= 10)
-                        _putchar('1');
-                        _putchar(j % 10 + '0');
-                }
-                        _putchar('\n');
-                }
-}
-
-
-
+#endif
