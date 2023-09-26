1d239c4bbf39b51016f9b41837a7a54bd3374a1f
David Ogola <Davidogola10@gmail.com>
2023-09-26 04:15:00 -0700

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,13 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h)
+	if (h== NULL)
+	Return (NULL);
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
