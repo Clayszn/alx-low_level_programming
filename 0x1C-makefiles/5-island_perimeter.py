9e761d95356ed58ed24592206ec69eadba8ba3ba
David ogola <Davidogola10@gmail.com>
2024-02-21 14:07:37 -0800

    Island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
-
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
-
-    The grid represents water by 0 and land by 1.
-    
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
     """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
-    
-    for i in range(height):
-        for j in range(width):
+    Calculate the perimeter of the island described in the grid.
+
+    Args:
+    - grid (list of list of integers): Represents the island grid where
+      0 represents a water zone and 1 represents a land zone.
+
+    Returns:
+    - int: The perimeter of the island.
+
+    Constraints:
+    - Grid cells are connected horizontally/vertically (not diagonally).
+    - Grid is rectangular, width and height don’t exceed 100.
+    - Grid is completely surrounded by water
+    - The island doesn’t have “lakes
+    """
+
+    perimeter = 0
+
+    # Iterate through each cell in the grid
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            # Check if the cell is part of the island (land)
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+                # Increment perimeter by 4 (sides of the cell)
+                perimeter += 4
+                # Chck neighbor cells and decrement perimeter for land cell
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Deduct 2 for the shared side
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Deduct 2 for the shared side
+
+    return perimeter
